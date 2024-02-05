bd37681d9bb77170805995190d8bb66688bb4f00
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 15:25:13 +0100

    LearnJava

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args){
 
         int x = 123;
-        float y = 3.14f;
+        double y = 3.14f;
         System.out.println("My number is: "+y);
     }
 }
