dd4c8a07c3ba1d45e018b93e96a30920a5019859
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 15:09:32 +0100

    LearnJava

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main{
     public static void main(String[] args){
 
-        int x = 123000000000000; //initialization
+        long x = 123000000000000; //initialization
 
         System.out.println("My number is: "+x);
     }
