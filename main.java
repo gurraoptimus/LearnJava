c651ac465c0f1cb24178fd5ccc8e7ea2df3a4d45
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 14:22:23 +0100

    LearnJava

--- main.java
+++ main.java
@@ -1,4 +1,7 @@
 public class main{
     public static void main(String[]args){
 
-        System.out
+        System.out.print("Learn Java\n");
+        System.out.print("is hard to Learning!");
+    }
+}
