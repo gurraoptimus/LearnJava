b261e97fb3580663b83b83d8ad4afddd80cba33c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 15:36:02 +0100

    LearnJava

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
         int x = 123;
         double y = 3.14;
         boolean z = false;
-        System.out.println(z);
+        char symbol = "@";
+        System.out.println(symbol);
     }
 }
