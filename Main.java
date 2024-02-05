40a7836c368838f26b3cf6a35d74659ad2662b23
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 15:33:29 +0100

    LearnJava

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main{
     public static void main(String[] args){
 
         int x = 123;
-        double y = 3.14f;
-        System.out.println("My number is: "+y);
+        double y = 3.14;
+        boolean z = false;
+        System.out.println(y);
     }
 }
