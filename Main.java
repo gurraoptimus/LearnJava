896509f1edea997c70167e28335268a875232ed9
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 14:44:55 +0100

    LearnJava

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+
 public class Main{
     public static void main(String[] args){
 
-        System.out.print("Learn Java\n");
-        System.out.print("can you Learning me about Java code's plz");
+        int x; //declartion
+
+
+        x = 123; //assignment
+
     }
 }
+
+
