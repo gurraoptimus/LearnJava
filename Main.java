d7f7d117978a79c3134f5223cd097fc583088b15
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-05 15:00:41 +0100

    LearnJava

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main{
     public static void main(String[] args){
 
-        int x = 123; //initialization
+        int x = 4455; //initialization
 
         System.out.println(x);
     }
