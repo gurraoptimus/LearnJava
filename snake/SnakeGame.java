97dbd90134270b6e9fd84a90b493fcc916e1f56b
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-08 14:16:16 +0100

    LearnJava

--- snake/SnakeGame.java
+++ snake/SnakeGame.java
@@ -5,5 +5,12 @@ import java.util.Random;
 import javax.swing.*;
 
 public class SnakeGame extends JPannel{
+    int boardWidth;
+    int boardHeight;
 
+    SnakeGame(int boardWidth, int boardHeight){
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPre
+    }
 }
