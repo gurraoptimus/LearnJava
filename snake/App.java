462369855d166740ea68fbba6b4a5b53035f64e8
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-08 15:12:18 +0100

    LearnJava

--- snake/App.java
+++ snake/App.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 
-public class app {
-    public static void main(String[] arge) throws Exception{
+public class App {
+    public static void main(String[] args) throws Exception {
         int boardWidth = 600;
         int boardHeight = boardWidth;
 
         JFrame frame = new JFrame("Snake");
         frame.setVisible(true);
-            frame.setSize(boardWidth, boardHeight);
+	frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
-        frame.setResizale(false);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
