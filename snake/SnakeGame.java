d7e5e583144cb273dce6dde71c1d5a8be9babe33
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-08 15:07:30 +0100

    LearnJava

--- snake/SnakeGame.java
+++ snake/SnakeGame.java
@@ -4,14 +4,52 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame extends JPannel{
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }  
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
+    
+    //snake
+    Tile snakeHead;
+    ArrayList<Tile> snakeBody;
+
+    //food
+    Tile food;
+    Random random;
+
+    //game logic
+    int velocityX;
+    int velocityY;
+    Timer gameLoop;
+
+    boolean gameOver = false;
 
     SnakeGame(int boardWidth, int boardHeight){
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimenion(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
+        addKeyListener(this);
+        setFocusable(true);
+
+        snakeHead = new Tile(5, 5);
+        snakeBody = new ArrayList<Tile>();
+
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
+
+        velocityX = 1;
+        velocityY = 0;
     }
 }
